3e95e70944756fb280118c7bc32ec7cec94dc448
Tucker <71043149+secretpanda-007@users.noreply.github.com>
2025-02-25 18:23:05 -0500

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PokemonBattle from './components/PokemonBattle';
+
+function App() {
+  return (
+    <div className="App">
+      <PokemonBattle />
+    </div>
+  );
+}
+
+export default App;
