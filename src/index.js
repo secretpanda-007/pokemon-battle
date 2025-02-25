5f66824a99d58cc57219a7160bbf6a8626e59e14
Tucker <71043149+secretpanda-007@users.noreply.github.com>
2025-02-25 17:20:44 -0500

    Update and rename index.js to index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PokemonBattle from './PokemonBattle';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <PokemonBattle />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
