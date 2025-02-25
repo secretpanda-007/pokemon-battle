47982e5e1b01682ade14432ba951e5a035547068
Tucker <71043149+secretpanda-007@users.noreply.github.com>
2025-02-25 16:48:35 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 
+// Prevent hydration issues with client-side only component
 const PokemonBattle = dynamic(() => import('../components/PokemonBattle'), { 
   ssr: false 
 });
 
 export default function Home() {
-  return (
-    <div className="min-h-screen bg-gray-50 py-8">
-      <div className="container mx-auto px-4">
-        <PokemonBattle />
-      </div>
-    </div>
-  );
+  return <PokemonBattle />;
 }
