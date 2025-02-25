247b1a4d2844667c0a20f67db08ece47267cb319
Tucker <71043149+secretpanda-007@users.noreply.github.com>
2025-02-25 16:42:26 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
+import React from 'react';
 import dynamic from 'next/dynamic';
 
-const PokemonBattle = dynamic(() => import('../components/PokemonBattle'), { ssr: false });
+// Use dynamic import with SSR disabled to prevent hydration issues with socket.io
+const PokemonBattle = dynamic(() => import('../components/PokemonBattle'), { 
+  ssr: false 
+});
 
 export default function Home() {
-  return <PokemonBattle />;
+  return (
+    <div className="min-h-screen bg-gray-50 py-8">
+      <div className="container mx-auto px-4">
+        <PokemonBattle />
+      </div>
+    </div>
+  );
 }
